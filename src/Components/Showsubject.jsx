175b4c34da2ba748ba017083fa5d3be0058bcd76
safeekur rahman a.z. <158799961+safrah710@users.noreply.github.com>
2024-09-10 06:27:21 +0530

    Update Showsubject.jsx

--- src/Components/Showsubject.jsx
+++ src/Components/Showsubject.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import toast from 'react-hot-toast';
 import { useParams } from 'react-router-dom';
-import useLogout from '../hooks/Uselogout';
+import useLogout from '../hooks/UseLogout';
 import { useNavigate } from 'react-router-dom';
 
 function Showsubject() {
