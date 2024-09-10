16b41f80278fd8a25094aec068c9874be124ab31
safeekur rahman a.z. <158799961+safrah710@users.noreply.github.com>
2024-09-10 06:27:48 +0530

    Update Viewres.jsx

--- src/Components/Viewres.jsx
+++ src/Components/Viewres.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import toast from 'react-hot-toast';
 import { useParams } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
-import useLogout from '../hooks/Uselogout';
+import useLogout from '../hooks/UseLogout';
 
 function Viewres() {
     let navigate = useNavigate();
