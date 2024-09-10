3df18716c09dc3f01c51d9806cb1360a3c451516
safeekur rahman a.z. <158799961+safrah710@users.noreply.github.com>
2024-09-10 06:27:37 +0530

    Update View.jsx

--- src/Components/View.jsx
+++ src/Components/View.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import toast from 'react-hot-toast';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import useLogout from '../hooks/Uselogout';
+import useLogout from '../hooks/UseLogout';
 function View() {
   const [name, setName] = useState("");
   const [age, setAge] = useState("");
